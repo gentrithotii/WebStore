6329cbc1407b698053a3d61f4fa861c1aae542bb
Annangggg <dnnguyen000@gmail.com>
2022-09-01 23:20:42 +0200

    Added very simple header

--- src/Home/Header.tsx
+++ src/Home/Header.tsx
@@ -1,14 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "/images/logo.png";
 
-function Header() {
-    const [count, setCount] = useState(0)
-  
-    return (
-    <>
-        <div><p>Header</p></div>
-    </>
-    )
-  }
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: "center"
+    },
+    logo: {
+      maxWidth: 40,
+      marginRight: '10px'
+    },
+    black: {
+        main: '#000000',
+        contrastText: '#fff',
+      },
+  })
+);
 
-  export default Header
+ function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt=" Fresh Fruits!" className={classes.logo} />
+          <Typography variant="h6" className={classes.title}>
+          <Button color="inherit">Products</Button>
+          </Typography>
+
+          <Button color="inherit">Cart</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
