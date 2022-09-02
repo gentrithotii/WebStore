6464d84164c364916712a74d9c26f00a5d880779
Hampus Andersson <hampe.01@hotmail.com>
2022-09-02 13:55:00 +0200

    Added product page

---
+++ src/Products/Header.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function Header() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <p>Header</p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
