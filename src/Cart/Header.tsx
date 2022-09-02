4eda7d5532251fb9baf52567393647df0757a1d9
Gentrithotii <Gentrithoti@hotmail.com>
2022-09-02 13:45:14 +0200

    Added Cart folders and flies

---
+++ src/Cart/Header.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function Header() {
+    const [count, setCount] = useState(0)
+  
+    return (
+    <>
+        <div><p>Footer</p></div>
+    </>
+    )
+  }
+
+  export default Header;
---
+++ src/Cart/Main.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function Main() {
+    const [count, setCount] = useState(0)
+  
+    return (
+    <>
+        <div><p>Footer</p></div>
+    </>
+    );
+  };
+
+  export default Main;
