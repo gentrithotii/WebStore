afaacf6bfa3680d7f1fb1d2991d089a107536d0f
Aalo29 <sarah.ali247@gmail.com>
2022-09-06 13:20:58 +0200

    Uppdate of checkoutpage

--- src/Checkout/Checkout.tsx
+++ src/Checkout/Checkout.tsx
@@ -1,15 +1,142 @@
-import { useState } from 'react'
-import Header from './Header'
-import Page from './Main'
-import Footer from './Footer'
 
 
 function Checkout() {
   return (
-    <div >
-      <h4>I am Checkout</h4>
+    <div > 
+      <div className="checkout">
+        <div className="container">
+          <h6>Home / Checkout</h6>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-7">
+                <div className="checkout__form">
+                  <h4>Customer information</h4>
+                  <div className = "card-body">
+                    <div className="row">
+                      <div className="col-md-6">
+                         <div className="form-group mb-3">
+                          <label>First Name</label>
+                          <input type="text" name="firstName" className="form-control"/>
+                          <div className="col-md-6">
+                         
+                         <div className="form-group mb-3">
+                          <label>Last Name</label>
+                          <input type="text" name="lastName" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>E-mail</label>
+                          <input type="text" name="email" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Phone</label>
+                          <input type="text" name="phone" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Address</label>
+                          <input type="text" name="address" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>City</label>
+                          <input type="text" name="city" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Country</label>
+                          <input type="text" name="country" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Zip Code</label>
+                          <input type="text" name="zipCode" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>State</label>
+                          <input type="text" name="state" className="form-control"/>
+                          <div className="form-group mb-3">                                                 
+                          <div className="form-group mb-3">
+                          <h4>Billing Adress</h4>
+                          <div className="form-group mb-3">
+                          <label>Address</label>
+                          <input type="text" name="address" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>City</label>
+                          <input type="text" name="city" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Country</label>
+                          <input type="text" name="country" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Zip Code</label>
+                          <input type="text" name="zipCode" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>State</label>
+                          <input type="text" name="state" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <h4>Payment</h4>
+                          <div className="form-group mb-3">
+                          <label>Card Number</label>
+                          <input type="text" name="cardNumber" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Card Name</label>
+                          <input type="text" name="cardName" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Card Expiry</label>
+                          <input type="text" name="cardExpiry" className="form-control"/>
+                          <div className="form-group mb-3">
+                          <label>Card CVV</label>
+                          <input type="text" name="cardCVV" className="form-control"/>
+                          
+
+                          
+
+                          
+                          
+
+                          
+                                                    
+
+
+                                        
+                         
+                       
+
+
+                       </div>
+</div>
+</div>
+
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+
+                    </div>
+                    </div>
+
+
+
+
+
+
+
+      
     </div>
-  )
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  
+
+  );
+  
 }
     
 
