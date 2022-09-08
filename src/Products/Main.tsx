2b170e150daa1e33b30235acca0f64f9f19dd4e0
Annangggg <dnnguyen000@gmail.com>
2022-09-08 19:38:24 +0200

    testing

--- src/Products/Main.tsx
+++ src/Products/Main.tsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { List } from "@mui/material";
 
-function Main() {
-  const [count, setCount] = useState(0);
-
+export default function Main() {
+  const cards = [1, 2, 3, 4, 5];
   return (
-    <>
-      <div>
-        <p>Main body</p>
-      </div>
-    </>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-evenly"
+      alignItems="center"
+    >
+      {cards.map(() => (
+        <Card sx={{ width: 200 }}>
+          <CardContent>
+            <div style={{ backgroundImage: "url(/images/Fruits/banana.jpg)", height: "100px" }}>
+            </div>
+          </CardContent>
+          <CardActions>
+            <List>
+              <Button size="small">Name</Button>
+              <Button size="small">Price</Button>
+              <Button size="small">Add to cart</Button>
+            </List>
+           
+          </CardActions>
+        </Card>
+      ))}
+    </Grid>
   );
 }
-
-export default Main;
