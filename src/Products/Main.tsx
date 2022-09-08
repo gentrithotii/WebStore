27d93eb55a53b0132219364c5af1fbd9b4fcbc47
Annangggg <dnnguyen000@gmail.com>
2022-09-08 19:41:07 +0200

    Revert "testing"
    This reverts commit 2b170e150daa1e33b30235acca0f64f9f19dd4e0.

--- src/Products/Main.tsx
+++ src/Products/Main.tsx
@@ -1,37 +1,15 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import { List } from "@mui/material";
+import { useState } from "react";
 
-export default function Main() {
-  const cards = [1, 2, 3, 4, 5];
+function Main() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-evenly"
-      alignItems="center"
-    >
-      {cards.map(() => (
-        <Card sx={{ width: 200 }}>
-          <CardContent>
-            <div style={{ backgroundImage: "url(/images/Fruits/banana.jpg)", height: "100px" }}>
-            </div>
-          </CardContent>
-          <CardActions>
-            <List>
-              <Button size="small">Name</Button>
-              <Button size="small">Price</Button>
-              <Button size="small">Add to cart</Button>
-            </List>
-           
-          </CardActions>
-        </Card>
-      ))}
-    </Grid>
+    <>
+      <div>
+        <p>Main body</p>
+      </div>
+    </>
   );
 }
+
+export default Main;
