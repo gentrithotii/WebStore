fc1abb4c791c62ade904354f0334c4a78095789e
Gentrithotii <Gentrithoti@hotmail.com>
2022-09-13 17:54:05 +0200

    Fixed when cart is empty

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,22 +1,24 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
-import { Stack } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
 import { CartItem } from "../component/CartItem";
 import { getData } from "../Products/main";
 import { Link } from "react-router-dom";
+import { homeContainer } from "../style/styles";
 
 const data = getData();
 
 export function Main() {
   const { cartItems } = useShoppingCart();
 
-  return (
+  if(cartItems.length !== 0){
+    return (
     <>
       <Stack gap={3}>
         {cartItems.map((item) => (
           <CartItem key={item.id} {...item} />
         ))}
         <div className="ms-auto fw-bold fs-5">
-          Total{" "}
+          Total :{" "}$
           {cartItems.reduce((total, cartItem) => {
             const item = data.find((i) => i.id === cartItem.id);
             return total + (item?.price || 0) * cartItem.quantity;
@@ -27,6 +29,23 @@ export function Main() {
         </Link>
       </Stack>
     </>
-  );
+  )
+  } 
+  else {
+    return(
+      <>
+       <Paper style={homeContainer.paperContainer}>
+                <div className="content rounded-border">
+                    <h2>The cart Is empty:</h2>
+                    <Link to="/Products">
+          <button><h2>Go back to products </h2></button>
+        </Link>
+
+                </div>
+            </Paper>
+      </>
+    );
+    
+  }
 }
 export default Main;
