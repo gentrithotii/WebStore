7cfdb1bc19b3136c84676ddb8f401f321b132917
Annangggg <dnnguyen000@gmail.com>
2022-09-13 15:18:53 +0200

    checkout styling

--- src/Checkout/Main.tsx
+++ src/Checkout/Main.tsx
@@ -1,35 +1,33 @@
-import { TextField } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Main() {
   const [] = useState("");
 
   return (
     <>
-      <div className="wrapper">
-        <br></br>
-        <br></br>
-        <br></br>
-        <form>
-          <input
-            required
-            name="FirstName"
-            type="text"
-            placeholder="First Name"
-          />
+      <div className="checkout-container">
+        <div className="div-container">
+        <form className="form">
+        <label>First Name</label>
+          <input required name="FirstName" type="text" placeholder="First Name"/>
+          <label >Last Name</label>
           <input required name="LastName" type="text" placeholder="Last Name" />
+          <label >Email</label>
           <input required name="Email" type="text" placeholder="Email" />
+          <label >Address</label>
           <input required name="Address" type="text" placeholder="Address" />
+          <label >Phone</label>
           <input required name="Phone" type="text" placeholder="Phone" />
 
           <br />
           <button type="submit" value="Submit">
             Submit
           </button>
-          <button type="button" value="Cancel">
-            Cancel
-          </button>
+
+          <Link to="/Cart"><button type="button" value="Cancel">Cancel</button></Link>
         </form>
+        </div>
       </div>
     </>
   );
