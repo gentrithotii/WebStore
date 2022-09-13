f40db354164ba82c8302c076caa52dffe59625ac
Hampus Andersson <hampe.01@hotmail.com>
2022-09-13 14:29:42 +0200

    Removed comments

--- src/Checkout/Main.tsx
+++ src/Checkout/Main.tsx
@@ -6,45 +6,6 @@ function Main() {
 
   return (
     <>
-      {/*  <h1>Checkout</h1>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="First Name"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Last Name"
-          variant="filled"
-        />
-        <div>
-          <TextField
-            required
-            id="filled-required"
-            label="Required"
-            defaultValue="Email"
-            variant="filled"
-          />
-        </div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Address"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <button>Submit</button>
-        <button>Cancel</button>
-      </div> */}
 
       <div className="wrapper">
         <br></br>
