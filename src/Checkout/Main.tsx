3a6526f3f103b5f02ceb30b196318f04c6fa0a9f
Hampus Andersson <hampe.01@hotmail.com>
2022-09-13 14:25:30 +0200

    Autofilling forms

--- src/Checkout/Main.tsx
+++ src/Checkout/Main.tsx
@@ -6,7 +6,7 @@ function Main() {
 
   return (
     <>
-      <h1>Checkout</h1>
+      {/*  <h1>Checkout</h1>
       <div>
         <TextField
           required
@@ -41,11 +41,36 @@ function Main() {
           variant="filled"
         />
       </div>
-        <div>
-            <button>Submit</button>
-            <button>Cancel</button>
-        
-        </div>
+      <div>
+        <button>Submit</button>
+        <button>Cancel</button>
+      </div> */}
+
+      <div className="wrapper">
+        <br></br>
+        <br></br>
+        <br></br>
+        <form>
+          <input
+            required
+            name="FirstName"
+            type="text"
+            placeholder="First Name"
+          />
+          <input required name="LastName" type="text" placeholder="Last Name" />
+          <input required name="Email" type="text" placeholder="Email" />
+          <input required name="Address" type="text" placeholder="Address" />
+          <input required name="Phone" type="text" placeholder="Phone" />
+
+          <br />
+          <button type="submit" value="Submit">
+            Submit
+          </button>
+          <button type="button" value="Cancel">
+            Cancel
+          </button>
+        </form>
+      </div>
     </>
   );
 }
