257a13d3c0e0eccc02ca12f4aebe5485171a89a6
Gentrithotii <Gentrithoti@hotmail.com>
2022-09-14 11:57:45 +0200

    flex

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -115,7 +115,7 @@ const Header: FC = (): ReactElement => {
             variant="outlined"
             className="rounded-circle"
           >
-            <svg style={{ margin: "0", maxWidth: "100%", width: "auto", height: "auto", display: "block",}}
+            <svg style={{ margin: "0", maxWidth: "100%", width: "auto", height: "auto", display: "flex",}}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 576 512"
               fill="white"
